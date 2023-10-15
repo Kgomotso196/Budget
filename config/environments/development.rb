34ccd33c4244f6440f54071ac0ecb228ea0e6d5e
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-10-15 09:44:23 +0200

    Add devise for user authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Do not eager load code on boot.
   config.eager_load = false
 
